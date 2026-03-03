4544c9df258794cf5d2b1015ca94805c7c286cd8
jhargrove09 <j.hargrove09@gmail.com>
2026-03-02 23:43:06 -0500

    corrected code for win/lose
    better script for win/lose

--- rock_paper_scissorrs.py
+++ rock_paper_scissorrs.py
@@ -5,5 +5,9 @@ if computer_choice == user_choice:
     print("TIE!!")
 elif user_choice == "rock" and computer_choice == "scissors":
     print("YOU WIN!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("YOU WIN!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("YOU WIN!")
 else:
-    print("you lose :(")
+    print("you lose :( , the computer WINS!! :)")
