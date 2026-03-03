e254d637a02cde9576b5bb07765bab36fb40df7f
jhargrove09 <j.hargrove09@gmail.com>
2026-03-02 22:45:22 -0500

    Added Years
    Added years as an option to display instead of a decimal number

--- age_calculator.py
+++ age_calculator.py
@@ -1,5 +1,6 @@
 age  = int(input("How old are you?\n"))
 
-decades = age/10
+decades = age // 10
+years = age % 10
 
-print("You are " + str(decades) + " decades old!!")
+print("You are " + str(decades) + " decades and " + str(years) + " years old!!")
