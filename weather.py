c1175ae0a37ae998c3237260737ce8dc944d3869
jhargrove09 <j.hargrove09@gmail.com>
2026-03-02 23:19:58 -0500

    added forecast
    updated script to include option for forecast

--- weather.py
+++ weather.py
@@ -2,13 +2,10 @@
 # temp == 95
 # print(temp == 95)
 
-temperature = 55
+temperature = 65
+forecast = "sunny"
 
-if temperature > 80:
-    print("It's too hot!")
-    print("Stay inside!")
-elif temperature < 60:
-    print("It's too cold!")
-    print("Stay inside!")
+if temperature < 80 and forecast != "rainy":
+    print("Go outside!")
 else:
-    print("Enjoy the outdoors!")
+    print("Stay inside!")
