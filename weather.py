08c79f1361309116fd7783b72bef2365bbb4abf8
jhargrove09 <j.hargrove09@gmail.com>
2026-03-02 23:04:24 -0500

    weather created
    created base for weather script

---
+++ weather.py
@@ -0,0 +1,14 @@
+# temp = 95
+# temp == 95
+# print(temp == 95)
+
+temperature = 55
+
+if temperature > 80:
+    print("It's too hot!")
+    print("Stay inside!")
+elif temperature < 60:
+    print("It's too cold!")
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
