01eb331bb9a7959faf7b4e93273d1aba09986845
jhargrove09 <j.hargrove09@gmail.com>
2026-03-03 23:31:01 -0500

    created loop
    added loop to enable the app to list out the full loan breakdown by months

--- loan.py
+++ loan.py
@@ -2,18 +2,19 @@
 money_owed = float(input('How much money do you owe?\n'))
 apr = float(input('What is the APR percentage of the loan?\n'))
 payment = float(input('How much is the monthly payment?\n'))
-months = float(input('How many months is the loan for?\n'))
+months = int(input('How many months is the loan for?\n'))
 
 monthly_rate = apr/100/12
 
-# Calculate interest to pay
-interest_paid = money_owed*monthly_rate
+for i in range(months):
+    # Calculate interest to pay
+    interest_paid = money_owed*monthly_rate
 
-# Add interest
-money_owed = money_owed + interest_paid
+    # Add interest
+    money_owed = money_owed + interest_paid
 
-# Make payment
-money_owed = money_owed - payment
+    # Make payment
+    money_owed = money_owed - payment
 
-print('Paid', payment, 'of which', interest_paid, 'was interest', end = ' ')
-print('Now I owe', money_owed)
+    print('Paid', payment, 'of which', interest_paid, 'was interest', end = ' ')
+    print('Now I owe', money_owed)
