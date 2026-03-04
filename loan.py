ea2683abf7cc2eaf0832c23e3edb825bd270104e
jhargrove09 <j.hargrove09@gmail.com>
2026-03-03 23:23:26 -0500

    loan calculator

---
+++ loan.py
@@ -0,0 +1,19 @@
+#Get details of load
+money_owed = float(input('How much money do you owe?\n'))
+apr = float(input('What is the APR percentage of the loan?\n'))
+payment = float(input('How much is the monthly payment?\n'))
+months = float(input('How many months is the loan for?\n'))
+
+monthly_rate = apr/100/12
+
+# Calculate interest to pay
+interest_paid = money_owed*monthly_rate
+
+# Add interest
+money_owed = money_owed + interest_paid
+
+# Make payment
+money_owed = money_owed - payment
+
+print('Paid', payment, 'of which', interest_paid, 'was interest', end = ' ')
+print('Now I owe', money_owed)
