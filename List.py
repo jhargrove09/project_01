b200114b1ea765fdc47948499ac1d22e40b1a177
jhargrove09 <j.hargrove09@gmail.com>
2026-03-03 22:58:43 -0500

    sum of list

--- List.py
+++ List.py
@@ -3,7 +3,10 @@ acronyms.append('JK')
 acronyms.append('OOTD')
 
 
-if 0 in [1, 2, 3, 4, 5]:
-    print('True')
-else:
-     print('False')
+#if 0 in [1, 2, 3, 4, 5]:
+#    print('True')
+#else:
+#     print('False')
+
+for ack in acronyms:
+     print(ack)
