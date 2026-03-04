a4eff62dbe8008c6cc232ed94e4f246806da25aa
jhargrove09 <j.hargrove09@gmail.com>
2026-03-04 00:00:52 -0500

    dictionary translations

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,19 @@
-acronyms = {'LOL' : 'Laugh Out Loud',
-            'IDK' : 'I Dont Know',
-            'SMH' : 'Shake My Head'}
+acronyms = {'LOL' : 'laugh out loud',
+            'IDK' : 'I dont know',
+            'SMH' : 'shake my head'}
 
 print(acronyms['LOL'])
+
+anime = {}
+
+anime['Eren Yeager'] = 'Attack On Titan'
+anime['Gon Freese'] = 'HunterXHunter'
+anime['Moa Moa'] = 'Apothocary Diaries'
+
+print(anime)
+
+sentence = 'IDK' + ' what happened the last episode of '+ 'Eren Yeager'
+translation = acronyms.get('IDK') + ' what happened on ' + anime.get('Eren Yeager')
+
+print(sentence)
+print(translation)
