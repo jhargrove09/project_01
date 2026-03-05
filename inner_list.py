14c966f04497ef6abafe39aeccdd977ec484b6d1
jhargrove09 <j.hargrove09@gmail.com>
2026-03-04 22:00:42 -0500

    printing list

--- inner_list.py
+++ inner_list.py
@@ -1,5 +1,7 @@
-menus = [['Fried Egg Sandwich', 'Cream Cheese Bagel', 'Coffee'],
-         ['BLT', 'Loxs with Capers', 'Turkey Deluxe Sandwich'],
-         ['Cheddar Brocolli Soup', 'Chicken Cesar Salad', 'Bolognese', 'Birra Tacos']]
+menus = {'Breakfast' : ['Fried Egg Sandwich', 'Cream Cheese Bagel', 'Coffee'],
+         'Lunch' : ['BLT', 'Loxs with Capers', 'Turkey Deluxe Sandwich'],
+         'Dinner' : ['Cheddar Brocolli Soup', 'Chicken Cesar Salad', 'Bolognese', 'Birra Tacos']}
 
-print(menus[0][1])
+print('Breakfast Menu:', menus['Breakfast'])
+print('Lunch Menu', menus['Lunch'])
+print('Dinner Menu', menus['Dinner'])
