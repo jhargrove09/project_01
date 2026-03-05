010f26817d3544119e6a5b6d42bf43a05e61f1fd
jhargrove09 <j.hargrove09@gmail.com>
2026-03-04 22:03:36 -0500

    dictionary loop

--- inner_list.py
+++ inner_list.py
@@ -2,6 +2,5 @@ menus = {'Breakfast' : ['Fried Egg Sandwich', 'Cream Cheese Bagel', 'Coffee'],
          'Lunch' : ['BLT', 'Loxs with Capers', 'Turkey Deluxe Sandwich'],
          'Dinner' : ['Cheddar Brocolli Soup', 'Chicken Cesar Salad', 'Bolognese', 'Birra Tacos']}
 
-print('Breakfast Menu:', menus['Breakfast'])
-print('Lunch Menu', menus['Lunch'])
-print('Dinner Menu', menus['Dinner'])
+for name, menu in menus.items():
+    print(name, ':', menu)
