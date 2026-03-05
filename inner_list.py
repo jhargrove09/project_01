f098fae18b9655671c7697fcb2cb7e9661b8782a
jhargrove09 <j.hargrove09@gmail.com>
2026-03-04 21:53:57 -0500

    inner list
    pulling data from inside list

---
+++ inner_list.py
@@ -0,0 +1,5 @@
+menus = [['Fried Egg Sandwich', 'Cream Cheese Bagel', 'Coffee'],
+         ['BLT', 'Loxs with Capers', 'Turkey Deluxe Sandwich'],
+         ['Cheddar Brocolli Soup', 'Chicken Cesar Salad', 'Bolognese', 'Birra Tacos']]
+
+print(menus[0][1])
