9086808e855f48e781721a953741539627e17858
jhargrove09 <j.hargrove09@gmail.com>
2026-03-05 22:08:47 -0500

    employee calc functioning

--- comparny.py
+++ comparny.py
@@ -7,14 +7,31 @@ class Company:
     def add_employee(self, new_employee):
         self.employees.append(new_employee)
 
+    def display_employees(self):
+        print('Current Employee:')
+        for i in self.employees:
+            print(i.fname,i.lname)
+        print('_________________________')
+
+    def pay_employees(self):
+        print('Paying Employees:')
+        for i in self.employees:
+            print('Paycheck for:', i.fname, i.lname)
+            print('Amount:', i.calculate_paycheck())
+            print('_________________________')
+
+
 def main():
     my_company = Company()
 
-    employee1 = Employee1('Eren', 'Yeager', '50000')
+    employee1 = Employee('Eren', 'Yeager', '50000')
     my_company.add_employee(employee1)
-    employee2= Employee2('Gon', 'Freese', '60000')
+    employee2= Employee('Gon', 'Freese', '60000')
     my_company.add_employee(employee2)
-    employee3= Employee3('Ichigo', 'Kurosaki', '70000')
+    employee3= Employee('Ichigo', 'Kurosaki', '70000')
     my_company.add_employee(employee3)
 
-    print(my_company.employees)
+    my_company.display_employees()
+
+main()
+
--- employee.py
+++ employee.py
@@ -1,7 +1,7 @@
 class Employee:
     def __init__(self, fname, lname, salary):
-        self.fname = fname_val
-        self.lname = lname_val
+        self.fname = fname
+        self.lname = lname
         self.salary = salary
     
     def calculate_paycheck(self):
