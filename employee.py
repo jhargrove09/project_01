83f535f6cbc80e78e88c07dbaee5f2d6dc41aa39
jhargrove09 <j.hargrove09@gmail.com>
2026-03-05 21:50:32 -0500

    employee

---
+++ employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, fname, lname, salary):
+        self.fname = fname_val
+        self.lname = lname_val
+        self.salary = salary
+    
+    def calculate_paycheck(self):
+        return self.salary/52
+    
