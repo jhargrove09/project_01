c6d7504440870e9d2b0e79c1f3407f0be04508b1
jhargrove09 <j.hargrove09@gmail.com>
2026-03-06 20:12:46 -0500

    robot complete

--- robot.py
+++ robot.py
@@ -10,4 +10,10 @@ class Robot:
         print('New position:', self.position)
 
 class Robot_Dog(Robot):
-    def make_noise(self)
+    def make_noise(self):
+        print('Woof Woof!')
+        
+        
+my_robot_dog = Robot_Dog('Bud')
+my_robot_dog.walk(10)
+my_robot_dog.make_noise()
